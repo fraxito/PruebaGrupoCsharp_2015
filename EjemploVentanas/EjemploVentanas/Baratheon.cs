dddde30dd32ac300365cb79d41b1d7840095208a
Jorge Cisneros <fraxito@gmail.com>
2015-04-22 08:28:55 +0200

    Ejemplo grupo DAM 2015
    Ejercicio en C# en grupo

---
+++ EjemploVentanas/EjemploVentanas/Baratheon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjemploVentanas
+{
+    public partial class Baratheon : Form
+    {
+        public Baratheon()
+        {
+            InitializeComponent();
+        }
+    }
+}
